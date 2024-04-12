85f107af5bcaa02d5f71858ce55e090b4eca54ee
MatteoStrazzera <mattestrazz@gmail.com>
2024-04-12 16:36:18 +0200

    Add message to check if PHP is working

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
+<?php
+
+$message= "PHP Badwords"
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <h1><?php echo $message ?></h1>
 </body>
 </html>
